8d96f40b8e4825751eaf70bf0970df5b3df0720d
victorrpf <victor.rpf@gmail.com>
2022-02-27 19:26:16 +0100

    versión comentada

--- geometria/src/Rectangulo_VRPF.java
+++ geometria/src/Rectangulo_VRPF.java
@@ -1,20 +1,40 @@
 //versión inicial
+/**
+ * 
+ * @author Víctor Ramón Pardilla Fernández
+ * @version 1.2
+ */
 public class Rectangulo_VRPF extends FiguraGeometrica_VRPF {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * 
+	 * @param tipoFigura
+	 * @param lG
+	 * @param lP
+	 */
 	public Rectangulo_VRPF(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+	
 	@Override
+	/**
+	 * Este método nos devuelve el área
+	 * @return área
+	 */
 	public double area() {
 		return l1 * l2;
 	}
 	
 	@Override
+	/**
+	 * Este método nos devuelve el perímetro
+	 * @return perímetro
+	 */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
